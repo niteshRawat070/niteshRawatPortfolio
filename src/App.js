d716d336450da703080fac8bd77ad27cf96e9c91
niteshRawat070 <74129269+niteshRawat070@users.noreply.github.com>
2021-03-03 10:27:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import Home from './screens/Home'
 import { AnimatePresence } from 'framer-motion';
 import { Route, Switch, useLocation } from "react-router-dom";
 import Skills from './screens/Skills';
+import {Helmet} from 'react-helmet'
 
 function App() {
   const location = useLocation();
 
   return (
     <>
+    <Helmet>
+          <title>Nitesh Rawat</title>
+              <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    </Helmet>
+        
           <Home />
           <Skills />
           </>
@@ -16,34 +22,3 @@ function App() {
 }
 
 export default App
-
-
-
-
-// import React from 'react'
-// import Home from './screens/Home'
-// import { AnimatePresence } from 'framer-motion';
-// import { Route, Switch, useLocation } from "react-router-dom";
-// import Skills from './screens/Skills';
-
-// function App() {
-//   const location = useLocation();
-
-//   return (
-//     <AnimatePresence exitBeforeEnter>
-//       <Switch location={location} key={location.key}>
-//           <Route path="/home">
-//           <Home />
-//           </Route>
-//           <Route path="/skills">
-//           <Skills />
-//           </Route>
-//           <Route path="/">
-//           <Home />
-//           </Route>
-//       </Switch>
-//     </AnimatePresence>
-//   )
-// }
-
-// export default App
